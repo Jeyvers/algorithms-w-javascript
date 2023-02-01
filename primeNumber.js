01a8d80149d9409e277c271ce28e17838ba78201
Jeyi Adole <meetjeyvers@gmail.com>
2023-02-01 13:11:59 +0100

    Updated Prime Number: My solution

--- primeNumber.js
+++ primeNumber.js
@@ -2,18 +2,30 @@
 // Prime numbers: whole numbers greater than 1 that are only divisible
 // by 1 and itself.
 
+// My Solution
 function isPrime(n) {
   let prime = true;
-  for (let i = 2; i < n; i++) {
-    // console.log(`Value: ${i}`, i * i, i * (i - 1));
-    if (i * i === n || i * (i - 1) === n) {
-      //   console.log(`${n} is not a prime number.`);
-      prime = false;
+  for (let i = 0; i < n; i++) {
+    for (let ii = 0; ii <= i; ii++) {
+      if (n === 1 || i * ii === n) {
+        prime = false;
+      }
     }
   }
   return prime;
 }
 
+// 2, 3, 5, 7, 11
+// 1,4,6,8,9,10
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
 console.log(isPrime(5));
 console.log(isPrime(6));
-console.log(isPrime(16));
+console.log(isPrime(7));
+console.log(isPrime(8));
+console.log(isPrime(9));
+console.log(isPrime(10));
+
+// TUTOR'S SOLUTION
