ab14104f46784ea24be783318f60c0dbeaddf9a9
Jeyi Adole <84779958+Jeyvers@users.noreply.github.com>
2023-02-26 14:12:53 +0000

    Tutor's solutions

--- index.js
+++ index.js
@@ -8,17 +8,42 @@ function isPowerOfTwo(n){
   }
   for(let i = 0; i <= Math.sqrt(n); i++) {
     if(Math.pow(2, i) === n) {
-      return ({is: true, syntax: `2^${i}`})
+      return true
     }
   }
 return false 
 }
+// Big O = O(sqrt(n))
+
+
+// TUTOR'S SOLUTION
+function isPowerOfTwoT(n) {
+  if(n<1) {
+    return false
+  }
+  while(n>1) {
+    if(n%2 !== 0) {
+      return false
+    } 
+    n=n/2
+  }
+  return true
+}
+// Big O = O(logn)
+
+
+// TUTOR'S BITWISE FUNCTION
+function isPowerOfTwoBitWise(n) {
+  if(n<1) {
+    return false
+  }
+  return (n & (n-1)) === 0
+}
+// Big O = O(1)
 
 // isPowerOfTwo(1) true (2^0) 
 // isPowerOfTwo(2) true (2^1) 
 // isPowerOfTwo(5) false
-console.log(Math.sqrt(5))
-console.log(isPowerOfTwo(1))
-console.log(isPowerOfTwo(2))
-console.log(isPowerOfTwo(5))
-console.log(isPowerOfTwo(16))
+console.log(isPowerOfTwoBitWise(1))
+console.log(isPowerOfTwoBitWise(2))
+console.log(isPowerOfTwoBitWise(5))
