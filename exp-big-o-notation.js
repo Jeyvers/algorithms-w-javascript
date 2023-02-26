e255a92f713303a193bf7ae9f98b94bf4e781de7
Jeyi Adole <84779958+Jeyvers@users.noreply.github.com>
2023-02-26 13:03:11 +0000

    Improved file structure, and changed the naming convention.

--- exp-big-o-notation.js
+++ exp-big-o-notation.js
@@ -24,3 +24,20 @@ function summation(n) {
 // O(n2) - Quadratic : 2 Nested loops
 // O(n3) - Cubic : 3 Nested loops
 // O(n3) - Logarithmic : Input size reduces by half every iteration
+
+
+// BIG O OBJECTS AND ARRAY EXPLANATION
+// An Object is a collection of key value pairs
+// Insert - O(1)
+// Remove - O(1)
+// Access - O(1)
+// Search - O(n)
+// Methods with linear time complexity
+// Object.keys, Object.values, Object.entries
+
+// An array is an ordered collection of values
+// Methods with constant
+// Insert / remove at the end O(1) Constant
+// Insert/remove at the beginning = O(n) Linear (Because the index has to be reset for all values)
+// Access - O(1) : Push/Pop
+// Search - O(n) : Shift/ unshift/ concat/slice/splice, forEach/map/filter/reduce
