2706d6395dc0fa7ee2638cd544f88ede58497813
Jeyi Adole <84779958+Jeyvers@users.noreply.github.com>
2023-02-26 14:40:44 +0000

    Recursion explanation

---
+++ exp-recursion.js
@@ -0,0 +1,11 @@
+// What is recursion?
+// Recursion is a problem solving technique where the solution depends on solutions to smaller instances of the same problem.
+// Recursion is when a function that call itself.
+
+// Why recursion?
+// It is a great technique to simplify a solution
+
+// ***
+// Every recursive solution needs to have a base case - a condition to terminate the recursion else you'll have an infinite loop.
+// Recursion might simplify solving teh problem but may not be a faster solution.
+// Recusion is a topic that is not the most straightforward to understand.
