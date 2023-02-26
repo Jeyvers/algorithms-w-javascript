7d35bcfeff205e0022a192c48a4358bc4d32d69c
Jeyi Adole <meetjeyvers@gmail.com>
2023-02-26 13:58:27 +0100

    EDITS

--- primeNumber.js
+++ primeNumber.js
@@ -43,6 +43,31 @@ function isPrimeT(n) {
 }
 // Big O = O(sqrt(n))
 
-console.log(isPrime(1)); // false
-console.log(isPrime(5)); // true
-console.log(isPrime(4)); // false
+// console.log(isPrime(1)); // false
+// console.log(isPrime(5)); // true
+// console.log(isPrime(4)); // false
+
+// const le = [34, 23, 1, 24, 75, 33, 54, 8];
+const le = [10, 20, 30];
+const k = 15;
+let store = 0;
+
+function runFunction() {
+  for (let i = 0; i < le.length; i++) {
+    const elementi = le[i];
+    for (let ii = 1; ii < le.length; ii++) {
+      const elementii = le[ii];
+      const solve = elementi + elementii;
+      if (solve < k && solve > store) {
+        store = solve;
+        console.log(i, ii, solve);
+      }
+    }
+  }
+  if (store > 0) {
+    return store;
+  } else return -1;
+  console.log(store);
+}
+
+console.log(runFunction());
