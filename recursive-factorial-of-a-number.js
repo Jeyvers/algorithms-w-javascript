97fe452b3cb86af351ef38e7fc820196a3be4538
Jeyi Adole <84779958+Jeyvers@users.noreply.github.com>
2023-03-19 13:35:36 +0000

    cleanup

--- recursive-factorial-of-a-number.js
+++ recursive-factorial-of-a-number.js
@@ -1,10 +1,9 @@
 function recursiveFactorial(n) {
-  if(n<2) {
-    return n
-  } 
-  console.log(n, n * recursiveFactorial(n-1))
-return n * recursiveFactorial(n-1)
+  if (n < 2) {
+    return 1
+  }
+  return n * recursiveFactorial(n - 1)
 }
 
-// console.log(recursiveFactorial(6))
-recursiveFactorial(4)
+console.log(recursiveFactorial(6))
+// recursiveFactorial(4)
