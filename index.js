3bd1464b34eba8d591551f562a9814d183839779
Jeyvers <>
2023-11-10 09:07:53 +0000

    Search algorithms completed, sort begins

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-// PROBLEM STATEMENT
-// Given a SORTED array of 'n' elements and a target element 'target', find the index of 't' in the array. Return -1 if the target element is not found.
-// e.g arr = [-5, 2, 10, 4, 6], t = 10 --- should return 4
-// Find middle element in the sorted array
-function binarySearch(arr, target) {
- let leftIndex = 0;
- let rightIndex = arr.length - 1;
+// Sorting Algorithms
+// Bubble sort, insertion sort, quick sort, merge sort. 
+// Given an array of integers, sort the array
 
- while (leftIndex <= rightIndex) {
-  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-  if (target === arr[middleIndex]) {
-   return middleIndex;
-  }
-  if (target < arr[middleIndex]) {
-   rightIndex = middleIndex - 1;
-  } else {
-   leftIndex = middleIndex + 1;
+// MY SOLUTION
+function bubbleSort(arr) {
+ let swapped = false;
+ for (let i = 0; i <= arr.length - 1; i++) {
+  let currentIndex = arr[i];
+  let nextIndex = arr[i + 1];
+  if (arr[i + 1] < arr[i]) {
+   arr[i] = nextIndex;
+   arr[i + 1] = currentIndex;
+   swapped = true;
   }
  }
- return -1;
+ if (swapped) {
+  return bubbleSort(arr);
+ } else {
+  return arr;
+ }
 }
 
-console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
-console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
-console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
 
-// TIme complexity O(logn) reduces each time
-
-
+console.log(bubbleSort([-6, 20, 8, -2, 4]))
+console.log(bubbleSort([30, 20,15, -10, 4]))
---
+++ recursive-binary-search.js
@@ -0,0 +1,32 @@
+// PROBLEM STATEMENT
+// Given a SORTED array of 'n' elements and a target element 'target', find the index of 't' in the array. Return -1 if the target element is not found.
+// e.g arr = [-5, 2, 10, 4, 6], t = 10 --- should return 4
+// Find middle element in the sorted array
+
+function recursiveBinarySearch(arr, target) {
+  return search(arr, target, 0, arr.length-1)
+
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+  if(leftIndex > rightIndex) {
+    return -1
+  }
+   let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if(target === arr[middleIndex]) {
+    return middleIndex
+  }
+  if(target < arr[middleIndex]) {
+    return search(arr, target, leftIndex, middleIndex-1)
+  } else {
+    return search(arr, target, middleIndex+1 , rightIndex)
+  }
+}
+
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)); // -1
+
+// Time complexity BigO = O(logn) reduces each time
+
+
