cad429d59cfc99d3e0f107c3bdcca36a441f146a
Jeyvers <>
2023-12-10 09:50:21 +0000

    Merge sort

--- 27-merge-sort.js
+++ 27-merge-sort.js
@@ -4,7 +4,7 @@ function mergeSort() {
   if(arr.length < 2) {
     return arr
   }
-  const mid = Math.floor(arar.length/2);
+  const mid = Math.floor(arr.length/2);
   const leftArr = arr.slice(0, mid);
   const rightArr = arr.slice(mid);
 
